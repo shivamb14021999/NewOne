700275800d1d2407f81f957930b460044c87f655
shivamb14021999 <shivamborasi1402@gmail.com>
2023-01-19 11:18:01 +0530

    123

---
+++ new123/src/main/java/com/example/demo/New123Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class New123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(New123Application.class, args);
+	}
+
+}
